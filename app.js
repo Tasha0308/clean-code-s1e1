1f97fb7de499f21d8842cecaca90d1caa408f27b
Tasha0308 <tashabeliatskaya@gmail.com>
2023-04-25 08:54:14 +0200

    refactor: change style completed task

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 var taskInput = document.getElementById("new-task");//Add a new task.
 var addButton = document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder = document.getElementById("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder = document.querySelector(".task-items_completed-tasks");//completed-tasks
+var completedTasksHolder = document.getElementById("completed-tasks");//completed-tasks
 
 
 //New task list item
